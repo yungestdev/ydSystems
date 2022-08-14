6a739addec4bc35175b3be38169e1d9c383f726f
yungestdev <80168738+yungestdev@users.noreply.github.com>
2022-08-14 15:49:33 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+title = `
+\t\t\t\t\t__   ______
+\t\t\t\t\t\\ \\ / /  _ \\ ___ _   _ ___
+\t\t\t\t\t \\ V /| | | / __| | | / __|
+\t\t\t\t\t  | | | |_| \\__ \\ |_| \\__ \\ 
+\t\t\t\t\t  |_| |____/|___/\\__, |___/
+\t\t\t\t\t                 |___/
+
+`
+console.log(title)
+console.log("%c\t\t\t\tThis web-app uses yungestDevSystems", "color: orange")
