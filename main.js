d221b18b16b7b9ff95b1ed6bf6c591f10a3e4879
yungestdev <80168738+yungestdev@users.noreply.github.com>
2022-09-06 01:20:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,30 @@ title = `
 \t\t\t\t\t  | | | |_| \\__ \\ |_| \\__ \\ 
 \t\t\t\t\t  |_| |____/|___/\\__, |___/
 \t\t\t\t\t                 |___/
-
 `
 console.log(title)
 console.log("%c\t\t\t\tThis web-app uses yungestDevSystems", "color: orange")
+
+window.localStorage.setItem("GCSE", navigator.userAgent);
+
+console.log("Running on:\n"+ window.localStorage.getItem("GCSE"));
+
+(function () {
+    (function a() {
+        try {
+            (function b(i) {
+                if (('' + (i / i)).length !== 1 || i % 20 === 0) {
+                    (function () { }).constructor('debugger')()
+                } else {
+                    debugger
+                }
+                b(++i)
+            }
+            )(0)
+        } catch (e) {
+            setTimeout(a, 5000)
+        }
+    }
+    )()
+}
+)();
